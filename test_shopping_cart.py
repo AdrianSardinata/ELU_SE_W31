835d3598cb449e4d1a30c4b9fa1f6dce57724e2e
Holmes <adrian.sardinata@elu.nl>
2025-06-16 19:03:39 +0800

    Fix pylint issues with test_shopping_cart.py

--- test_shopping_cart.py
+++ test_shopping_cart.py
@@ -1,27 +1,31 @@
+""""
+A simple test suite to verify the stdout of shopping_cart.py
+"""
+
 import unittest
-import sys
 import io
 from unittest.mock import patch
 
 class TestShoppingCart(unittest.TestCase):
-	def test_shopping_cart_01(self):
-		"""
-		Executes shopping_cart.py script and asserts its printed output.
-		"""
-		gold_stdout = (
-			"Item: Item A - Price: $10.99\n"
-			"Item: Item B - Price: $5.99\n"
-			"Item: Item C - Price: $8.49\n"
-			"Total price: 25.47\n"
-		)
-		file = open('shopping_cart.py')
-		code = file.read()
-			
-		with patch('sys.stdout', new_callable=io.StringIO) as mock_stdout:
-			exec(code, globals(), locals())
-			stdout_str = mock_stdout.getvalue()
-			self.assertEqual(stdout_str, gold_stdout)
-		file.close()
+    """For end-testing shopping_cart.py"""
+    def test_shopping_cart_01(self):
+        """
+        Executes shopping_cart.py script and asserts its printed output.
+        """
+        gold_stdout = (
+            "Item: Item A - Price: $10.99\n"
+            "Item: Item B - Price: $5.99\n"
+            "Item: Item C - Price: $8.49\n"
+            "Total price: 25.47\n"
+        )
+        file = open('shopping_cart.py')
+        code = file.read()
+            
+        with patch('sys.stdout', new_callable=io.StringIO) as mock_stdout:
+            exec(code, globals(), locals())
+            stdout_str = mock_stdout.getvalue()
+            self.assertEqual(stdout_str, gold_stdout)
+        file.close()
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
